e9150d960b2223299831b8adf16d3d18ea87e9a9
Sasha125588 <155098366+Sasha125588@users.noreply.github.com>
2024-09-08 19:27:51 +0300

    1

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        blue: '#3c5ba9',
+        main: '#45464f'
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
